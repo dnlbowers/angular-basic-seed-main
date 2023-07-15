3cdcd74bdb01f178c762a82e7af1f3b61ba65fed
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-07-15 08:39:21 +0200

    complete lesson on scoped styles in components

--- src/app/admin/components/donut-card/donut-card.component.ts
+++ src/app/admin/components/donut-card/donut-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import { Donut } from "../../models/donut.model";
 
 @Component({
   selector: 'app-donut-card',
@@ -16,8 +17,35 @@ import { Component } from '@angular/core';
     </div>
   `,
   styles: [
+    `
+      .donut-card {
+        display: flex;
+        align-items: center;
+        background: #f7f7f7;
+        border-radius: 5px;
+        margin-bottom: 5px;
+        padding: 5px 15px;
+        transition: transform 0.2s ease-in-out;
+        &:hover {
+          transform: translateY(-3px);
+        }
+        &-name {
+          font-size: 16px;
+        }
+        &-price {
+          font-size: 14px;
+          color: #c14583;
+        }
+        &-icon {
+          width:50px;
+          margin-right: 10px;
+        }
+      }
+    `
   ]
 })
 export class DonutCardComponent {
+
+  @Input() donut!: Donut;
 
 }
