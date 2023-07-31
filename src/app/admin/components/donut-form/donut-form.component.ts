e0a5ec92db90b9f96086ba17cf63110bb35ec048
David Bowers <dbowers@greentube.com>
2023-07-31 08:27:45 +0200

    add select menu

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -10,6 +10,16 @@ import { Component } from '@angular/core';
       </label>
 
       <label>
+        <span>Icon</span>
+        <select
+        name="icon"
+        class="input input--select"
+        ngModel>
+          <option *ngFor="let icon of icons" [ngValue]="icon">{{ icon }}</option>
+        </select>
+      </label>
+
+      <label>
         <span>Price</span>
         <input type="number" name="price"class="input" ngModel >
       </label>
@@ -55,5 +65,13 @@ import { Component } from '@angular/core';
   ]
 })
 export class DonutFormComponent {
-
+  icons: string[] = [
+    'caramel-swirl',
+    'glazed-fudge',
+    'just-chocolate',
+    'sour-supreme',
+    'strawberry-glaze',
+    'vanilla-sundae',
+    'zesty-lemon'
+  ]
 }
