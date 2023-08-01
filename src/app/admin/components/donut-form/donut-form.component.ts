0487deeb1c294723d809c6d4ad9f3033a0808333
David Bowers <dbowers@greentube.com>
2023-08-01 10:46:24 +0200

    form status and required feilds

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
     <form class=donut-form #form="ngForm">
       <label>
         <span>Name</span>
-        <input type="text" name="name" class="input" ngModel>
+        <input type="text" name="name" class="input" required ngModel>
       </label>
 
       <label>
@@ -21,29 +21,29 @@ import { Component } from '@angular/core';
 
       <label>
         <span>Price</span>
-        <input type="number" name="price"class="input" ngModel >
+        <input type="number" name="price"class="input" required ngModel >
       </label>
 
       <div class="donut-form-radios">
         <p class="donut-form-radios-label">Promo:</p>
         <label>
-          <input type="radio" name="promo" [value]="undefined" ngModel>
+          <input type="radio" name="promo" required [value]="undefined" ngModel>
           <span>None</span>
         </label>
         <label>
-          <input type="radio" name="promo" value="new" ngModel>
+          <input type="radio" name="promo" required value="new" ngModel>
           <span>New</span>
         </label>
         <label>
-          <input type="radio" name="promo" value="limited" ngModel>
+          <input type="radio" name="promo" required value="limited" ngModel>
           <span>Limited</span>
         </label>
       </div>
       <label>
         <span>Description</span>
-        <textarea name="description" class="input input--textarea" ngModel></textarea>
+        <textarea name="description" class="input input--textarea" required ngModel></textarea>
       </label>
-      <pre>{{ form.value | json }}</pre>
+      <pre>{{ form.form.status | json }}</pre>
     </form>
   `,
   styles: [
