ca5b2039669311f5ad9be1f9ffc374e57db5d054
David Bowers <dbowers@greentube.com>
2023-08-02 16:40:50 +0200

    form validation properties

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
     <form class=donut-form #form="ngForm">
       <label>
         <span>Name</span>
-        <input type="text" name="name" class="input" required ngModel>
+        <input type="text" name="name" class="input" required ngModel #name="ngModel">
+        <p>{{name.valid }}</p>
+        <p>{{name.invalid }}</p>
+        <p>{{name.touched }}</p>
+        <p>{{name.untouched }}</p>
+        <p>{{name.pristine }}</p>
+        <p>{{name.dirty }}</p>
       </label>
 
       <label>
@@ -43,7 +49,7 @@ import { Component } from '@angular/core';
         <span>Description</span>
         <textarea name="description" class="input input--textarea" required ngModel></textarea>
       </label>
-      <pre>{{ form.form.status | json }}</pre>
+      <pre>{{ form.value | json }}</pre>
     </form>
   `,
   styles: [
