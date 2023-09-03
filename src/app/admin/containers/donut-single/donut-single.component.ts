53c9ce945e72b97958fd4441dfa9140fdf749fda
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-09-03 10:22:29 +0200

    connect up delete functionality

--- src/app/admin/containers/donut-single/donut-single.component.ts
+++ src/app/admin/containers/donut-single/donut-single.component.ts
@@ -25,7 +25,7 @@ export class DonutSingleComponent implements  OnInit {
 
   ngOnInit(): void {
     this.donutService
-      .readOne('8amkz9')
+      .readOne('Y42KQwL')
       .subscribe((donut: Donut) => ( this.donut = donut));
   }
 
@@ -43,7 +43,7 @@ export class DonutSingleComponent implements  OnInit {
   }
 
   onDelete(donut: Donut) {
-    this.donutService.delete(donut);
+    this.donutService.delete(donut).subscribe(() => console.log('Deleted Successfully!!'));
   }
 
 }
